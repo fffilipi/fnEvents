9e17d4300cc8b560d789fa404099265a971467f3
Filipi Nata <filipi_f@hotmail.com>
2022-12-26 23:27:41 -0300

    Adicionado flash Messages para sucesso de cliente cadastrado

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,6 +30,6 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,8 +42,16 @@
             </div>
         </nav>
      </header>
-    @yield('content')
-
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg"> {{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
     <footer>
         <p>fnevents &copy; 2022</p>
     </footer>
