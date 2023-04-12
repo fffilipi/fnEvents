7c1dd47c8d6979465b991a1c29a7f667e77582b5
Filipi Nata <filipi_f@hotmail.com>
2023-04-11 23:14:39 -0300

    create update events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -87,4 +87,36 @@ class EventController extends Controller
 
     }
 
+    public function edit($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.edit', ['event' => $event]);
+
+    }
+
+    public function update(Request $request) {
+
+        $data = $request->all();
+
+         //image Upload
+         if($request->hasFile('image') && $request->file('image')->isValid()) {
+            
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now") . "." . $extension); // cria um rash baseado no nome original e horario 
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evendo editado com sucesso!');
+
+    }
+
 }
